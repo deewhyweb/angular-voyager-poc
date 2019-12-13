bf292c9e47f24b76181d0940a7fd9f4d1f0e45b3
Philip Hayes <phayes@redhat.com>
2019-12-13 11:22:42 -0500

    Added sample graphql query

--- src/index.js
+++ src/index.js
@@ -1,8 +1,47 @@
 import {
-  createClient, VoyagerClient, DataSyncConfig,
-  OfflineQueueListener, ConflictListener, AuthContextProvider
-} from '@aerogear/voyager-client';
-angular.module("app", []).controller("HelloWorldCtrl", function($scope) {  
-     
-    $scope.message="Hello World" 
-    } )
+  createClient,
+  VoyagerClient,
+  DataSyncConfig,
+  OfflineQueueListener,
+  ConflictListener,
+  AuthContextProvider
+} from "@aerogear/voyager-client";
+import gql from "graphql-tag";
+angular.module("app", []).controller("HelloWorldCtrl", function($scope) {
+  $scope.message = "Hello World";
+  const config = {
+    httpUrl: "http://localhost:8001/graphql",
+    wsUrl: "ws://localhost:8001/graphql"
+  };
+
+  createClient(config).then(client => {
+    const options = gql`
+      query allTasks {
+        allTasks {
+          name
+          created
+        }
+      }
+    `;
+
+    client
+      .query({
+        fetchPolicy: "network-only",
+        query: gql`
+          query allTasks {
+            allTasks {
+              name
+            }
+          }
+        `
+      })
+      //Print the response of the query
+      .then(({ data }) => {
+        setTimeout(() => {
+          $scope.message = data.allTasks[0].name;
+          $scope.$apply();
+        },0)
+        
+      });
+  });
+});
